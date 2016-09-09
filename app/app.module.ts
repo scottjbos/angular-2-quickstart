300ea97fed7618114ba12a980c3e125238ed54da
Scott Bos <scott.j.bos@gmail.com>
2016-09-09 02:12:28 -0400

    Stage 7 HTTP - added the necessary dependencies to use HTTP in our application. - refactored HeroService to load heroes from a web API. - extended HeroService to support post, put and delete methods. - updated components to allow adding, editing and deleting of heroes. - configured an in-memory web API.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,27 +1,39 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroService }         from './hero.service';
-import { routing } from './app.routing';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroSearchComponent }  from './hero-search.component';
+import { HeroService }          from './hero.service';
+import { routing }              from './app.routing';
+
+import './rxjs-extensions';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    HeroSearchComponent
   ],
   providers: [
-    HeroService
+    HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
